9e71466918c8aac9f0a1010d9fbd3f9f43f61bc3
R-type <nortype@gmail.com>
2014-11-27 16:14:46 +0100

    ARM fix1

--- sources/src/include/memory_uae.h
+++ sources/src/include/memory_uae.h
@@ -62,7 +62,7 @@ extern uae_u32 wait_cpu_cycle_read_ce020 (uaecptr addr, int mode);
 extern void wait_cpu_cycle_write_ce020 (uaecptr addr, int mode, uae_u32 v);
 
 #undef DIRECT_MEMFUNCS_SUCCESSFUL
-#ifndef ANDPORT
+#if !defined(ANDPORT) && !defined(ARM)
 #include "machdep/maccess.h"
 #else
 #include "machdep/maccess-generic.h"
