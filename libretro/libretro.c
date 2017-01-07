f89de4e8cf0d92bad952d4fee110ed991a92c2a0
twinaphex <libretro@gmail.com>
2017-01-07 07:12:04 +0100

    Add ftime implementation for Android

--- libretro/libretro.c
+++ libretro/libretro.c
@@ -409,3 +409,29 @@ void retro_cheat_set(unsigned index, bool enabled, const char *code)
    (void)enabled;
    (void)code;
 }
+
+#ifdef ANDROID
+#include <sys/timeb.h>
+
+int ftime(struct timeb *tb)
+{
+    struct timeval  tv;
+    struct timezone tz;
+
+    if (gettimeofday (&tv, &tz) < 0)
+        return -1;
+
+    tb->time    = tv.tv_sec;
+    tb->millitm = (tv.tv_usec + 500) / 1000;
+
+    if (tb->millitm == 1000)
+    {
+        ++tb->time;
+        tb->millitm = 0;
+    }
+    tb->timezone = tz.tz_minuteswest;
+    tb->dstflag  = tz.tz_dsttime;
+
+    return 0;
+}
+#endif
