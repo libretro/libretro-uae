1abecddd9627402d1fabeeab65055ae81dd3c7f0
DamsRoxxx <walzdamien@hotmail.com>
2018-09-01 21:47:26 +0200

    Disk and HDD direct launch
    Only '.uae' files were supported as argument.
    
    Disk image (adf|dms|fdi|ipf|zip) and HDD image (hdf|hdz) were not.
    
    Now, when as disk or HDD image is provided as argument, the core generate a temporary '.uae' config file in save directory and launch it.
    
    Disk and HDD image need a 'kick.rom' in system dir to be launched.
    
    HDD game need WHDLoad.hdf in system directory to work.
    
    Documentation will be updated.

--- libretro/libretro.c
+++ libretro/libretro.c
@@ -3,6 +3,8 @@
 #include "retrodep/retroglue.h"
 #include "libretro-mapper.h"
 #include "libretro-glue.h"
+#include "retro_files.h"
+#include "retro_strings.h"
 #include "sources/src/include/uae_types.h"
 
 #define EMULATOR_DEF_WIDTH 640
@@ -66,13 +68,52 @@ static struct retro_input_descriptor input_descriptors[] = {
    { 255, 255, 255, 255, NULL }
 };
 
+const char *retro_save_directory;
+const char *retro_system_directory;
+const char *retro_content_directory;
+
+// Amiga default models
+
+#define A500 " \
+cpu_type=68000\n \
+chipmem_size=2\n \
+bogomem_size=7\n \
+chipset=ocs\n"
+
+#define A600 " \
+cpu_type=68000\n \
+chipmem_size=2\n \
+fastmem_size=4\n \
+chipset=ecs\n"
+
+#define A1200 " \
+cpu_type=68ec020\n \
+chipmem_size=4\n \
+fastmem_size=8\n \
+chipset=aga\n"
+
+static char uae_config[1024];
 
 void retro_set_environment(retro_environment_t cb)
 {
    struct retro_variable variables[] = {
-     { "resolution", "Internal resolution; 640x400|640x432|640x480|640x540|704x480|704x540|720x480|720x540|800x600|1024x768", },
-     { "analog","Use Analog; OFF|ON", },
-     { "leds","Leds; Standard|Simplified|None", },
+     { "puae_model", "Model; A600|A1200|A500", },
+     { "puae_resolution", "Internal resolution; 720x568|640x400|640x432|640x480|640x540|704x480|704x540|720x480|720x540|800x600|1024x768", }, // PUAE recommanded resolution is 720x568
+     { "puae_analog","Use Analog; OFF|ON", },
+     { "puae_leds","Leds; Standard|Simplified|None", },
+     { "puae_cpu_speed", "CPU speed; real|max", },
+     { "puae_cpu_compatible", "CPU compatible; true|false", },
+     { "puae_sound_output", "Sound output; normal|exact|interrupts|none", },
+     { "puae_sound_frequency", "Sound frequency; 44100|22050|11025", },
+     { "puae_sound_channels", "Sound channels; stereo|mixed|mono", },
+     { "puae_sound_interpol", "Sound interpolation; none|rh|crux|sync", },
+     { "puae_floppy_speed", "Floppy speed; 100|200|300|400|500|600|700|800", },
+     { "puae_immediate_blits", "Immediate blit; false|true", },
+     { "puae_ntsc", "NTSC Mode; false|true", },
+     { "puae_gfx_linemode", "Line mode; double|none", }, // Removed scanline, we have shaders for this
+     { "puae_gfx_correct_aspect", "Correct aspect ratio; true|false", },
+     { "puae_gfx_center_vertical", "Vertical centering; simple|smart|none", },
+     { "puae_gfx_center_horizontal", "Horizontal centering; simple|smart|none", },
      { NULL, NULL },
    };
   /*
@@ -86,9 +127,10 @@ void retro_set_environment(retro_environment_t cb)
 
 static void update_variables(void)
 {
+	uae_config[0] = '\0';
    struct retro_variable var = {0};
 
-   var.key = "resolution";
+   var.key = "puae_resolution";
    var.value = NULL;
 
    if (environ_cb(RETRO_ENVIRONMENT_GET_VARIABLE, &var) && var.value)
@@ -105,7 +147,7 @@ static void update_variables(void)
          defaulth = strtoul(pch, NULL, 0);
    }
 
-   var.key = "analog";
+   var.key = "puae_analog";
    var.value = NULL;
 
    if (environ_cb(RETRO_ENVIRONMENT_GET_VARIABLE, &var) && var.value)
@@ -117,7 +159,7 @@ static void update_variables(void)
       ledtype = 1;
    }
    
-   var.key = "leds";
+   var.key = "puae_leds";
    var.value = NULL;
 
    if (environ_cb(RETRO_ENVIRONMENT_GET_VARIABLE, &var) && var.value)
@@ -134,6 +176,149 @@ static void update_variables(void)
       {
         ledtype = 2;  
       }
+   }
+
+   var.key = "puae_model";
+   var.value = NULL;
+
+   if (environ_cb(RETRO_ENVIRONMENT_GET_VARIABLE, &var) && var.value)
+   {
+		if (strcmp(var.value, "A500") == 0)
+			strcat(uae_config, A500);
+		if (strcmp(var.value, "A600") == 0)
+			strcat(uae_config, A600);
+		if (strcmp(var.value, "A1200") == 0)
+			strcat(uae_config, A1200);
+   }
+
+   var.key = "puae_cpu_speed";
+   var.value = NULL;
+
+   if (environ_cb(RETRO_ENVIRONMENT_GET_VARIABLE, &var) && var.value)
+   {
+		strcat(uae_config, "cpu_speed=");
+		strcat(uae_config, var.value);
+		strcat(uae_config, "\n");
+   }
+
+   var.key = "puae_cpu_compatible";
+   var.value = NULL;
+
+   if (environ_cb(RETRO_ENVIRONMENT_GET_VARIABLE, &var) && var.value)
+   {
+		strcat(uae_config, "cpu_compatible=");
+		strcat(uae_config, var.value);
+		strcat(uae_config, "\n");
+   }
+
+   var.key = "puae_sound_output";
+   var.value = NULL;
+
+   if (environ_cb(RETRO_ENVIRONMENT_GET_VARIABLE, &var) && var.value)
+   {
+		strcat(uae_config, "sound_output=");
+		strcat(uae_config, var.value);
+		strcat(uae_config, "\n");
+   }
+
+   var.key = "puae_sound_frequency";
+   var.value = NULL;
+
+   if (environ_cb(RETRO_ENVIRONMENT_GET_VARIABLE, &var) && var.value)
+   {
+		strcat(uae_config, "sound_frequency=");
+		strcat(uae_config, var.value);
+		strcat(uae_config, "\n");
+   }
+
+   var.key = "puae_sound_channels";
+   var.value = NULL;
+
+   if (environ_cb(RETRO_ENVIRONMENT_GET_VARIABLE, &var) && var.value)
+   {
+		strcat(uae_config, "sound_channels=");
+		strcat(uae_config, var.value);
+		strcat(uae_config, "\n");
+   }
+
+   var.key = "puae_sound_interpol";
+   var.value = NULL;
+
+   if (environ_cb(RETRO_ENVIRONMENT_GET_VARIABLE, &var) && var.value)
+   {
+		strcat(uae_config, "sound_interpol=");
+		strcat(uae_config, var.value);
+		strcat(uae_config, "\n");
+   }
+
+   var.key = "puae_floppy_speed";
+   var.value = NULL;
+
+   if (environ_cb(RETRO_ENVIRONMENT_GET_VARIABLE, &var) && var.value)
+   {
+		strcat(uae_config, "floppy_speed=");
+		strcat(uae_config, var.value);
+		strcat(uae_config, "\n");
+   }
+
+   var.key = "puae_immediate_blits";
+   var.value = NULL;
+
+   if (environ_cb(RETRO_ENVIRONMENT_GET_VARIABLE, &var) && var.value)
+   {
+		strcat(uae_config, "immediate_blits=");
+		strcat(uae_config, var.value);
+		strcat(uae_config, "\n");
+   }
+
+   var.key = "puae_ntsc";
+   var.value = NULL;
+
+   if (environ_cb(RETRO_ENVIRONMENT_GET_VARIABLE, &var) && var.value)
+   {
+		strcat(uae_config, "ntsc=");
+		strcat(uae_config, var.value);
+		strcat(uae_config, "\n");
+   }
+
+   var.key = "puae_gfx_linemode";
+   var.value = NULL;
+
+   if (environ_cb(RETRO_ENVIRONMENT_GET_VARIABLE, &var) && var.value)
+   {
+		strcat(uae_config, "gfx_linemode=");
+		strcat(uae_config, var.value);
+		strcat(uae_config, "\n");
+   }
+
+   var.key = "puae_gfx_correct_aspect";
+   var.value = NULL;
+
+   if (environ_cb(RETRO_ENVIRONMENT_GET_VARIABLE, &var) && var.value)
+   {
+		strcat(uae_config, "gfx_correct_aspect=");
+		strcat(uae_config, var.value);
+		strcat(uae_config, "\n");
+   }
+
+   var.key = "puae_gfx_center_vertical";
+   var.value = NULL;
+
+   if (environ_cb(RETRO_ENVIRONMENT_GET_VARIABLE, &var) && var.value)
+   {
+		strcat(uae_config, "gfx_center_vertical=");
+		strcat(uae_config, var.value);
+		strcat(uae_config, "\n");
+   }
+
+   var.key = "puae_gfx_center_horizontal";
+   var.value = NULL;
+
+   if (environ_cb(RETRO_ENVIRONMENT_GET_VARIABLE, &var) && var.value)
+   {
+		strcat(uae_config, "gfx_center_horizontal=");
+		strcat(uae_config, var.value);
+		strcat(uae_config, "\n");
    }
 }
 
@@ -161,6 +346,39 @@ static void retro_wrap_emulator(void)
 void retro_init(void)
 {
    enum retro_pixel_format fmt = RETRO_PIXEL_FORMAT_RGB565;
+
+	const char *system_dir = NULL;
+
+	if (environ_cb(RETRO_ENVIRONMENT_GET_SYSTEM_DIRECTORY, &system_dir) && system_dir)
+	{
+	  // if defined, use the system directory			
+	  retro_system_directory=system_dir;		
+	}		   
+
+	const char *content_dir = NULL;
+
+	if (environ_cb(RETRO_ENVIRONMENT_GET_CONTENT_DIRECTORY, &content_dir) && content_dir)
+	{
+	  // if defined, use the system directory			
+	  retro_content_directory=content_dir;		
+	}			
+
+	const char *save_dir = NULL;
+
+	if (environ_cb(RETRO_ENVIRONMENT_GET_SAVE_DIRECTORY, &save_dir) && save_dir)
+	{
+	  // If save directory is defined use it, otherwise use system directory
+	  retro_save_directory = *save_dir ? save_dir : retro_system_directory;      
+	}
+	else
+	{
+	  // make retro_save_directory the same in case RETRO_ENVIRONMENT_GET_SAVE_DIRECTORY is not implemented by the frontend
+	  retro_save_directory=retro_system_directory;
+	}
+
+   printf("Retro SYSTEM_DIRECTORY %s\n",retro_system_directory);
+   printf("Retro SAVE_DIRECTORY %s\n",retro_save_directory);
+   printf("Retro CONTENT_DIRECTORY %s\n",retro_content_directory);
 
    memset(key_state, 0, sizeof(key_state));
    memset(key_state2, 0, sizeof(key_state2));
@@ -213,7 +431,7 @@ void retro_get_system_info(struct retro_system_info *info)
    info->library_version  = "v2.6.1";
    info->need_fullpath    = true;
    info->block_extract    = false;	
-   info->valid_extensions = "adf|dms|fdi|ipf|zip|uae";
+   info->valid_extensions = "adf|dms|fdi|ipf|zip|hdf|hdz|uae";
 }
 
 void retro_get_system_av_info(struct retro_system_av_info *info)
@@ -316,6 +534,18 @@ sortie:
    co_switch(emuThread);
 }
 
+#define ADF_FILE_EXT "adf"
+#define FDI_FILE_EXT "fdi"
+#define DMS_FILE_EXT "dms"
+#define IPF_FILE_EXT "ipf"
+#define ZIP_FILE_EXT "zip"
+#define HDF_FILE_EXT "hdf"
+#define HDZ_FILE_EXT "hdz"
+#define UAE_FILE_EXT "uae"
+#define LIBRETRO_PUAE_CONF "puae_libretro.uae"
+#define KICK_ROM "kick.rom"
+#define WHDLOAD_HDF "WHDLoad.hdf"
+
 bool retro_load_game(const struct retro_game_info *info)
 {
   int w = 0, h = 0;
@@ -323,31 +553,117 @@ bool retro_load_game(const struct retro_game_info *info)
   environ_cb(RETRO_ENVIRONMENT_SET_INPUT_DESCRIPTORS, input_descriptors);
 
   RPATH[0] = '\0';
-
+  
   if (*info->path)
-    {
-      const char *full_path = (const char*)info->path;
-      strncpy(RPATH, full_path, sizeof(RPATH));
+  {
+	  // If argument is a disk or hard drive image file file
+	  if(		strendswith((const char*)info->path, ADF_FILE_EXT)
+			||	strendswith((const char*)info->path, FDI_FILE_EXT)
+			||	strendswith((const char*)info->path, DMS_FILE_EXT)
+			||	strendswith((const char*)info->path, IPF_FILE_EXT)
+			||	strendswith((const char*)info->path, ZIP_FILE_EXT)
+			||	strendswith((const char*)info->path, HDF_FILE_EXT)
+			||	strendswith((const char*)info->path, HDZ_FILE_EXT))
+	  {
+			printf("Game '%s' is a disk or hard drive image file.\n", (const char*)info->path);
+			
+			// Init kickstart
+			char kickstart[RETRO_PATH_MAX];
+			path_join((char*)&kickstart, retro_system_directory, KICK_ROM);
+			
+			// Verifiy kickstart
+			if(!file_exists(kickstart))
+			{
+				// Kickstart rom not found
+				fprintf(stderr, "Kickstart rom '%s' not found.\n", (const char*)&kickstart);
+				fprintf(stderr, "You must have a kickstart file ('kick.rom') in your RetroArch system directory to launch an disk or hard drive image file.\n");
+				return false;	  
+			}
 
-      // checking parsed file for custom resolution
-      FILE * configfile;
+			path_join((char*)&RPATH, retro_save_directory, LIBRETRO_PUAE_CONF);
+			printf("Generating temporary uae config file '%s'.\n", (const char*)&RPATH);
 
-      char filebuf[4096];
-      if((configfile = fopen (RPATH, "r")))
-	{
-	  while(fgets(filebuf, sizeof(filebuf), configfile))
-	    {
-	      sscanf(filebuf,"gfx_width = %d",&w);
-	      sscanf(filebuf,"gfx_height = %d",&h);
-	    }
-	  fclose(configfile);
-	}
-    }
+			// Open tmp config file
+			FILE * configfile;
+			if((configfile = fopen(RPATH, "w")))
+			{
+				// Write common config
+				fprintf(configfile, uae_config);
+				fprintf(configfile, "show_leds=true\n"); // FIXME : Bug if led are not shown ? (example hang in Rick Dangerous 2).
+				fprintf(configfile, "kickstart_rom_file=%s\n", (const char*)&kickstart);
 
-  if (w<=0 || h<=0 || w>EMULATOR_MAX_WIDTH || h>EMULATOR_MAX_HEIGHT) {
+				// If argument is a hard drive image file
+				if(		strendswith((const char*)info->path, HDF_FILE_EXT)
+					||	strendswith((const char*)info->path, HDZ_FILE_EXT))
+				{
+					// Init WHDLoad
+					char whdload[RETRO_PATH_MAX];
+					path_join((char*)&whdload, retro_system_directory, WHDLOAD_HDF);
+					
+					// Verifiy WHDLoad
+					if(!file_exists(whdload))
+					{
+						// WHDLoad image not found
+						fprintf(stderr, "WHDLoad image file '%s' not found.\n", (const char*)&whdload);
+						fprintf(stderr, "You must have a WHDLoad image file ('WHDLoad.hdf') in your RetroArch system directory to launch a hard drive image file.\n");
+						fclose(configfile);
+						return false;	  
+					}
+					
+					// Write hard drives informations					
+					fprintf(configfile, "hardfile=read-write,32,1,2,512,%s\n", (const char*)&whdload);
+					fprintf(configfile, "hardfile=read-write,32,1,2,512,%s\n", (const char*)info->path);
+				}
+				else
+				{
+					// Write floppy information
+					fprintf(configfile, "floppy0=%s\n", (const char*)info->path);
+				}
+				fclose(configfile);
+			}
+			else
+			{
+				// Error
+				fprintf(stderr, "Error while writing '%s' file.\n", (const char*)&RPATH);
+				return false;	  
+			}	  
+	  }
+	  // If argument is an uae file
+	  else if(strendswith((const char*)info->path, UAE_FILE_EXT))
+	  {
+			printf("Game '%s' is an UAE config file.\n", (const char*)info->path);
+		  
+			const char *full_path = (const char*)info->path;
+			strncpy(RPATH, full_path, sizeof(RPATH));
+
+			// checking parsed file for custom resolution
+			FILE * configfile;
+
+			char filebuf[4096];
+			if((configfile = fopen (RPATH, "r")))
+			{
+				while(fgets(filebuf, sizeof(filebuf), configfile))
+				{
+				  sscanf(filebuf,"gfx_width = %d",&w);
+				  sscanf(filebuf,"gfx_height = %d",&h);
+				}
+				fclose(configfile);
+			}
+	  }
+	  // Other extensions
+	  else
+	  {
+			// Unsupported file format
+			fprintf(stderr, "Content '%s'. Unsupported file format.\n", (const char*)info->path);
+			return false;	  
+	  }
+  }
+  
+  if (w<=0 || h<=0 || w>EMULATOR_MAX_WIDTH || h>EMULATOR_MAX_HEIGHT) 
+  {
     w = defaultw;
     h = defaulth;
-    }
+  }
 
   fprintf(stderr, "[libretro-uae]: resolution selected: %dx%d (default: %dx%d)\n", w, h, defaultw, defaulth);
 
