e79d67bd732a7bef0d1912aa0926a34902c2308e
DamsRoxxx <walzdamien@hotmail.com>
2019-05-30 11:43:04 +0200

    Fix : IPF support through CAPSIMG on windows
    IPF support through CAPSIMG was broken on windows.
    
    The code was only for linux.
    
    Ported some function from https://github.com/FrodeSolheim/fs-uae to make this work

---
+++ sources/src/caps/uae_dlopen.c
@@ -0,0 +1,56 @@
+#include "sysconfig.h"
+#include "sysdeps.h"
+#include "uae_api.h"
+#include "uae_dlopen.h"
+#include "writelog.h"
+
+#ifdef _WIN32
+#include "windows.h"
+#else
+#include <dlfcn.h>
+#endif
+
+UAE_DLHANDLE uae_dlopen(const TCHAR *path)
+{
+	UAE_DLHANDLE result;
+	if (path == NULL || path[0] == _T('\0')) {
+		write_log(_T("DLOPEN: No path given\n"));
+		return NULL;
+	}
+#ifdef _WIN32
+	result = LoadLibrary(path);
+#else
+	result = dlopen(path, RTLD_NOW);
+	const char *error = dlerror();
+	if (error != NULL)  {
+		write_log("DLOPEN: %s\n", error);
+	}
+#endif
+	if (result == NULL) {
+		write_log("DLOPEN: Failed to open %s\n", path);
+	}
+	return result;
+}
+
+void *uae_dlsym(UAE_DLHANDLE handle, const char *name)
+{
+#if 0
+	if (handle == NULL) {
+		return NULL;
+	}
+#endif
+#ifdef _WIN32
+	return (void *) GetProcAddress(handle, name);
+#else
+	return dlsym(handle, name);
+#endif
+}
+
+void uae_dlclose(UAE_DLHANDLE handle)
+{
+#ifdef _WIN32
+	FreeLibrary (handle);
+#else
+	dlclose(handle);
+#endif
+}
