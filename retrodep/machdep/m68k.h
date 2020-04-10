3bcb4ce8632c615f9d5ec1879b089d16406f3e49
rsn8887 <rsn8887@users.noreply.github.com>
2020-04-09 23:12:56 -0500

    ANDROID X86: Fix build

--- retrodep/machdep/m68k.h
+++ retrodep/machdep/m68k.h
@@ -1,6 +1,6 @@
 #if defined (__x86_64__)
 #include "../../sources/src/md-amd64-gcc/m68k.h"
-#elif defined (__i386__)
+#elif defined (__i386__) && !defined(__PIC__)
 #include "../../sources/src/md-i386-gcc/m68k.h"
 #elif defined(__powerpc__)
 #include "../../sources/src/md-ppc-gcc/m68k.h"
