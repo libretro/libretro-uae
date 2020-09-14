6580a7055e9109a74bf6486535fd4405b33b55b8
Tony Jansson <tony@pihaus.org>
2020-09-14 23:59:19 +0300

    Clean-up unzip logging

--- retrodep/retroglue.c
+++ retrodep/retroglue.c
@@ -845,7 +845,7 @@ void remove_recurse(const char *path)
             continue;
 
         sprintf(filename, "%s%s%s", path, DIR_SEP_STR, dirp->d_name);
-        fprintf(stdout, "Temp clean: %s\n", filename);
+        fprintf(stdout, "Clean: %s\n", filename);
 
         if (path_is_directory(filename))
             remove_recurse(filename);
@@ -925,7 +925,7 @@ void zip_uncompress(char *in, char *out, char *lastfile)
 
         if ((*filename_withoutpath) == '\0')
         {
-            fprintf(stdout, "Unzip mkdir:   %s\n", filename_withpath);
+            fprintf(stdout, "Mkdir: %s\n", filename_withpath);
             path_mkdir(filename_withpath);
         }
         else
@@ -952,7 +952,7 @@ void zip_uncompress(char *in, char *out, char *lastfile)
 
             if (fout != NULL)
             {
-                fprintf(stdout, "Unzip extract: %s\n", write_filename);
+                fprintf(stdout, "Unzip: %s\n", write_filename);
 
                 do
                 {
