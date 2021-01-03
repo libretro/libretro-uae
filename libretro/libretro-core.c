4e376491cb6cce5c9a487c45a95f68ee856aa1ba
sonninnos <tony@pihaus.org>
2021-01-03 10:29:04 +0200

    Config file hard disk reordering

--- libretro/libretro-core.c
+++ libretro/libretro-core.c
@@ -4124,36 +4124,27 @@ static bool retro_create_config()
                tmp_str = string_replace_substring(full_path, "\\", "\\\\");
                char tmp_str_name[RETRO_PATH_MAX];
                char tmp_str_path[RETRO_PATH_MAX];
+               snprintf(tmp_str_name, sizeof(tmp_str_name), "%s", path_basename(tmp_str));
+               path_remove_extension(tmp_str_name);
+
                if (strendswith(full_path, "slave") || strendswith(full_path, "info"))
                {
-                  snprintf(tmp_str_name, sizeof(tmp_str_name), "%s", path_basename(tmp_str));
-                  path_remove_extension(tmp_str_name);
                   path_parent_dir(tmp_str);
                   snprintf(tmp_str_path, sizeof(tmp_str_path), "%s%s", tmp_str, tmp_str_name);
                   if (!path_is_directory(tmp_str_path))
                      snprintf(tmp_str_path, sizeof(tmp_str_path), "%s", tmp_str);
                }
                if (strendswith(full_path, "lha"))
-                  fprintf(configfile, "filesystem2=ro,DH0:LHA:\"%s\",0\n", (const char*)tmp_str);
+                  fprintf(configfile, "filesystem2=ro,DH0:%s:\"%s\",0\n", tmp_str_name, tmp_str);
                else if (path_is_directory(full_path))
-                  fprintf(configfile, "filesystem2=rw,DH0:%s:\"%s\",0\n", path_basename(tmp_str), (const char*)tmp_str);
+                  fprintf(configfile, "filesystem2=rw,DH0:%s:\"%s\",0\n", tmp_str_name, tmp_str);
                else if (strendswith(full_path, "slave") || strendswith(full_path, "info"))
                   fprintf(configfile, "filesystem2=rw,DH0:%s:\"%s\",0\n", tmp_str_name, tmp_str_path);
                else
-                  fprintf(configfile, "hardfile2=rw,DH0:\"%s\",32,1,2,512,0,,uae1\n", (const char*)tmp_str);
+                  fprintf(configfile, "hardfile2=rw,DH0:\"%s\",32,1,2,512,0,,uae1\n", tmp_str);
                free(tmp_str);
                tmp_str = NULL;
 
-               /* Attach retro_system_directory as a read only hard drive for WHDLoad kickstarts/prefs/key */
-#ifdef WIN32
-               tmp_str = string_replace_substring(retro_system_directory, "\\", "\\\\");
-               fprintf(configfile, "filesystem2=ro,RASystem:RASystem:\"%s\",-128\n", (const char*)tmp_str);
-               free(tmp_str);
-               tmp_str = NULL;
-#else
-               /* Force the ending slash to make sure the path is not treated as a file */
-               fprintf(configfile, "filesystem2=ro,RASystem:RASystem:\"%s%s\",-128\n", retro_system_directory, "/");
-#endif
                /* WHDSaves HDF mode */
                if (opt_use_whdload == 2)
                {
@@ -4313,6 +4304,17 @@ static bool retro_create_config()
                      fclose(whdload_prefs);
                   }
                }
+
+               /* Attach retro_system_directory as a read only hard drive for WHDLoad kickstarts/prefs/key */
+#ifdef WIN32
+               tmp_str = string_replace_substring(retro_system_directory, "\\", "\\\\");
+               fprintf(configfile, "filesystem2=ro,RASystem:RASystem:\"%s\",-128\n", (const char*)tmp_str);
+               free(tmp_str);
+               tmp_str = NULL;
+#else
+               /* Force the ending slash to make sure the path is not treated as a file */
+               fprintf(configfile, "filesystem2=ro,RASystem:RASystem:\"%s%s\",-128\n", retro_system_directory, "/");
+#endif
             }
             else
             {
