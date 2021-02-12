e90965f2e27183fb5e968b2b14fa6a42e6115ac7
sonninnos <tony@pihaus.org>
2021-02-12 23:32:00 +0200

    Statusline adjustments

--- retrodep/gui.c
+++ retrodep/gui.c
@@ -59,53 +59,39 @@ void gui_cd_led (int led)
 
 static void gui_flicker_led2 (int led, int unitnum, int status)
 {
-	static int resetcounter[LED_MAX];
-	uae_s8 old;
-	uae_s8 *p;
+    static int resetcounter[LED_MAX];
+    uae_s8 old;
+    uae_s8 *p;
 
-	if (led == LED_HD)
-		p = &gui_data.hd;
-	else if (led == LED_CD)
-		p = &gui_data.cd;
-	else if (led == LED_MD)
-		p = &gui_data.md;
-	else if (led == LED_NET)
-		p = &gui_data.net;
-	else
-		return;
-	old = *p;
-	if (status < 0) {
-		if (old < 0) {
-			gui_led (led, -1);
-		} else {
-			gui_led (led, 0);
-		}
-		return;
-	}
-	if (status == 0 && old < 0) {
-		*p = 0;
-		resetcounter[led] = 0;
-		gui_led (led, 0);
-		return;
-	}
-	if (status == 0) {
-		resetcounter[led]--;
-		if (resetcounter[led] > 0)
-			return;
-	}
-#ifdef RETROPLATFORM
-	if (unitnum >= 0) {
-		if (led == LED_HD) {
-			rp_hd_activity(unitnum, status ? 1 : 0, status == 2 ? 1 : 0);
-		} else if (led == LED_CD) {
-			rp_cd_activity(unitnum, status);
-		}
-	}
-#endif
-	*p = status;
-	resetcounter[led] = 4;
-	if (old != *p)
-		gui_led (led, *p);
+    if (led == LED_HD)
+        p = &gui_data.hd;
+    else if (led == LED_CD)
+        p = &gui_data.cd;
+    else if (led == LED_MD)
+        p = &gui_data.md;
+    else if (led == LED_NET)
+        p = &gui_data.net;
+    else
+        return;
+    old = *p;
+
+    if (status == 0 && old < 0) {
+        *p = 0;
+        resetcounter[led] = 0;
+        return;
+    }
+
+    if (status == 0 && led == LED_CD && old == LED_CD_AUDIO)
+        resetcounter[led] = 0;
+
+    if (status == 0) {
+        resetcounter[led]--;
+        if (resetcounter[led] > 0)
+            return;
+    }
+    *p = status;
+
+    resetcounter[led] = 5;
 }
 
 void gui_flicker_led (int led, int unitnum, int status)
@@ -113,8 +99,10 @@ void gui_flicker_led (int led, int unitnum, int status)
     if (led < 0) {
         if (gui_data.hd >= 0)
             gui_flicker_led2(LED_HD, 0, 0);
+#if 0
         if (gui_data.cd >= 0)
             gui_flicker_led2(LED_CD, 0, 0);
+#endif
         if (gui_data.net >= 0)
             gui_flicker_led2(LED_NET, 0, 0);
         if (gui_data.md >= 0)
