6e3e1f7efcbc1975d24d5922cc120f091977142f
sonninnos <tony@pihaus.org>
2023-04-14 17:35:10 +0300

    VKBD cleanups

--- libretro/libretro-vkbd.c
+++ libretro/libretro-vkbd.c
@@ -44,7 +44,7 @@ static long last_press_time = 0;
 static bool let_go_of_button = true;
 static long last_press_time_button = 0;
 static int vkey_pressed = -1;
-static int vkey_sticky = -1;
+static int vkey_sticky  = -1;
 static int vkey_sticky1 = -1;
 static int vkey_sticky2 = -1;
 
@@ -578,8 +578,9 @@ void print_vkbd(void)
    }
 
    /* Opacity */
-   BKG_ALPHA = (retro_vkbd_transparent) ?
-         ((BKG_ALPHA == GRAPH_ALPHA_100) ? GRAPH_ALPHA_100 : GRAPH_ALPHA_75) : GRAPH_ALPHA_100;
+   BKG_ALPHA = (retro_vkbd_transparent)
+         ? ((BKG_ALPHA == GRAPH_ALPHA_100) ? GRAPH_ALPHA_100 : GRAPH_ALPHA_75)
+         : GRAPH_ALPHA_100;
 
    /* Selected key */
    int current_key_pos = (vkey_pos_y * VKBDX) + vkey_pos_x + page;
@@ -663,17 +664,17 @@ void print_vkbd(void)
 
    /* Gap backgrounds */
    if (VKBDX_GAP_POS)
-      draw_fbox(XOFFSET+XBASEKEY+(VKBDX_GAP_POS*XSIDE)+FONT_WIDTH, vkbd_y_min-FONT_HEIGHT,
-                vkbd_x_gap_pad-FONT_WIDTH, vkbd_y_max-vkbd_y_min+(FONT_HEIGHT*2),
+      draw_fbox(XOFFSET+XBASEKEY+(VKBDX_GAP_POS * XSIDE)+FONT_WIDTH, vkbd_y_min-FONT_HEIGHT,
+                vkbd_x_gap_pad-FONT_WIDTH, vkbd_y_max-vkbd_y_min+(FONT_HEIGHT * 2),
                 0, BRD_ALPHA);
 
    if (VKBDY_GAP_POS)
    {
-      draw_fbox(vkbd_x_min-FONT_WIDTH, YOFFSET+YBASEKEY+(VKBDY_GAP_POS*YSIDE)+FONT_HEIGHT,
+      draw_fbox(vkbd_x_min-FONT_WIDTH, YOFFSET+YBASEKEY+(VKBDY_GAP_POS * YSIDE)+FONT_HEIGHT,
                 vkbd_x_max-vkbd_x_min+FONT_WIDTH+XKEYSPACING+FONT_WIDTH, vkbd_y_gap_pad-FONT_HEIGHT,
                 0, BRD_ALPHA);
 #if 0
-      draw_fbox(XOFFSET+XBASEKEY+(VKBDX_GAP_POS*XSIDE)+vkbd_x_gap_pad, YOFFSET+YBASEKEY+(VKBDY_GAP_POS*YSIDE)+FONT_HEIGHT,
+      draw_fbox(XOFFSET+XBASEKEY+(VKBDX_GAP_POS * XSIDE)+vkbd_x_gap_pad, YOFFSET+YBASEKEY+(VKBDY_GAP_POS * YSIDE)+FONT_HEIGHT,
                 XSIDE+FONT_WIDTH, vkbd_y_gap_pad-FONT_HEIGHT,
                 0, BRD_ALPHA);
 #endif
@@ -766,13 +767,11 @@ static void input_vkbd_sticky(void)
    {
       vkey_sticky1_release = 0;
       vkey_sticky1 = -1;
-      retro_key_up(vkey_sticky1);
    }
    if (vkey_sticky2_release)
    {
       vkey_sticky2_release = 0;
       vkey_sticky2 = -1;
-      retro_key_up(vkey_sticky2);
    }
 }
 
@@ -1039,8 +1038,8 @@ void toggle_vkbd(void)
 
 void input_vkbd(void)
 {
-   long now = 0;
-   unsigned int i = 0;
+   long now  = 0;
+   uint8_t i = 0;
 
    input_vkbd_sticky();
 
@@ -1060,12 +1059,6 @@ void input_vkbd(void)
    }
 
    now = retro_ticks() / 1000;
-
-   /* VKBD Mouse acceleration */
-   const int mspeed_default = 2;
-   static int mspeed;
-   if (!vkflag[RETRO_DEVICE_ID_JOYPAD_B])
-      mspeed = mspeed_default;
 
    /* Absolute pointer */
    int p_x = input_state_cb(0, RETRO_DEVICE_POINTER, 0, RETRO_DEVICE_ID_POINTER_X);
@@ -1104,6 +1097,12 @@ void input_vkbd(void)
       printf("px:%d py:%d (%d,%d) vkey:%dx%d\n", p_x, p_y, px, py, vkey_pos_x, vkey_pos_y);
 #endif
    }
+
+   /* VKBD Mouse acceleration */
+   const int mspeed_default = 2;
+   static int mspeed;
+   if (!vkflag[RETRO_DEVICE_ID_JOYPAD_B])
+      mspeed = mspeed_default;
 
    /* Press Return, RetroPad Start */
    i = RETRO_DEVICE_ID_JOYPAD_START;
@@ -1272,18 +1271,18 @@ void input_vkbd(void)
                   if (retro_capslock)
                      retro_key_down(RETROK_LSHIFT);
 
-                  if (!vkey_sticky)
-                  {
-                     if (vkey_pressed == vkey_sticky1)
-                        vkey_sticky1_release = 1;
-                     if (vkey_pressed == vkey_sticky2)
-                        vkey_sticky2_release = 1;
-                  }
-                  retro_key_down(vkey_pressed);
+                  if (vkey_pressed == vkey_sticky1)
+                     vkey_sticky1_release = 1;
+                  if (vkey_pressed == vkey_sticky2)
+                     vkey_sticky2_release = 1;
+
+                  if (vkey_pressed != vkey_sticky1 && vkey_pressed != vkey_sticky2)
+                     retro_key_down(vkey_pressed);
                }
                break;
          }
       }
+
       last_vkey_pressed = vkey_pressed;
       last_vkey_pressed_time = now;
    }
