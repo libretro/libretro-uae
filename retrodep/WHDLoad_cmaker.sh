6a60fca8210d33527769a5772a3255276821c82b
sonninnos <tony@pihaus.org>
2023-04-28 01:22:05 +0300

    WHDLoad LHA launch compatibility improvements

--- retrodep/WHDLoad_cmaker.sh
+++ retrodep/WHDLoad_cmaker.sh
@@ -1,17 +1,17 @@
 cd ../whdload/WHDLoad_files/
-zip -9 -r ../WHDLoad_files.zip .
+zip -9 -r -X ../WHDLoad_files.zip .
 cd ../../retrodep/
 xxd -i ../whdload/WHDLoad_files.zip WHDLoad_files.zip.c
 rm ../whdload/WHDLoad_files.zip
 
-gzip -9 -f -k ../whdload/WHDLoad.hdf 
+gzip -9 -f -k -N ../whdload/WHDLoad.hdf
 xxd -i ../whdload/WHDLoad.hdf.gz WHDLoad_hdf.gz.c
 rm ../whdload/WHDLoad.hdf.gz
 
-gzip -9 -f -k ../whdload/WHDSaves.hdf 
+gzip -9 -f -k -N ../whdload/WHDSaves.hdf
 xxd -i ../whdload/WHDSaves.hdf.gz WHDSaves_hdf.gz.c
 rm ../whdload/WHDSaves.hdf.gz
 
-gzip -9 -f -k ../whdload/WHDLoad.prefs
+gzip -9 -f -k -N ../whdload/WHDLoad.prefs
 xxd -i ../whdload/WHDLoad.prefs.gz WHDLoad_prefs.gz.c
 rm ../whdload/WHDLoad.prefs.gz
