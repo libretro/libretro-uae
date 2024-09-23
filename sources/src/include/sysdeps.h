07488c3aa356d9aca1a18f42553d641fee9248f1
sonninnos <tony@pihaus.org>
2024-09-23 19:20:44 +0300

    Buildfixing

--- sources/src/include/sysdeps.h
+++ sources/src/include/sysdeps.h
@@ -744,6 +744,16 @@ typedef int64_t off64_t;
 typedef uint32_t uint32;
 typedef uint8_t uint8;
 
+typedef uint64_t UINT64;
+typedef uint32_t UINT32;
+typedef uint16_t UINT16;
+typedef uint8_t UINT8;
+
+typedef int64_t INT64;
+typedef int32_t INT32;
+typedef int16_t INT16;
+typedef int8_t INT8;
+
 #include "misc.h"
 
 #ifdef VITA
