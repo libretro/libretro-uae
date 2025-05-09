f2850ffcc850b33eb0e5dff7f170549f6423c71d
sonninnos <tony@pihaus.org>
2025-05-10 01:34:05 +0300

    Add legacy note for keyboard pass-through option

--- libretro/libretro-core.c
+++ libretro/libretro-core.c
@@ -2097,7 +2097,7 @@ static void retro_set_core_options()
          "puae_physical_keyboard_pass_through",
          "Input > Keyboard Pass-through",
          "Keyboard Pass-through",
-         "'ON' passes all physical keyboard events to the core. 'OFF' prevents RetroPad keys from generating keyboard events.",
+         "'ON' passes all physical keyboard events to the core. 'OFF' prevents RetroPad keys from generating keyboard events.\nNOTE: This is a legacy option for old frontends that do not block keyboard events when using RetroPad, so it does nothing with current RetroArch.",
          NULL,
          "input",
          {
